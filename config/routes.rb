daec965d25e0033b9edaf36003bf9f1faae3fe5a
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-02-26 16:05:03 +0100

    all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, only: [:new, :create, :show, :update] do
+
+  resources :cocktails, only: [:index, :new, :create, :show, :update] do
     resources :doses, only: [:create]
   end
 
