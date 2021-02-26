f67d095cd5fe12222afa464d186c264986f4d412
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-02-26 20:05:52 +0100

    Add CSS

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :update, :destroy]
 
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
   end
 
@@ -24,12 +24,21 @@ class CocktailsController < ApplicationController
   end
 
   def update
-    @cocktail = Cocktail.find(params[:id])
     @cocktail.update(cocktail_params)
     redirect_to cocktail_path(@cocktail)
   end
 
+  def destroy
+    @cocktail.destroy
+
+    redirect_to cocktails_path, notice: 'cocktail was successfully destroyed.'
+  end
+
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
